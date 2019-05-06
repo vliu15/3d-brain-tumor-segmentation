48c2df8bb5bac9e296e2f8714e0437271abafc60
Vincent Liu <vliu15@stanford.edu>
2019-05-06 16:27:44 -0700

    Decoder docstrings

--- model/decoder.py
+++ model/decoder.py
@@ -7,9 +7,35 @@ class DecoderBlock(tf.keras.layers.Layer):
     def __init__(self,
                  filters,
                  input_shape,
-                 data_format='channels_last',
                  kernel_size=(3, 3, 3),
+                 data_format='channels_last',
                  groups=8):
+        """Initializes one level of upsampling in the convolutional decoder.
+
+            Each decoder block consists of a pointwise 3D-convolution followed
+            by a 3D-upsampling. The output of these 2 layers is summed with
+            the corresponding channel-depth output from the encoder and fed
+            into a convolutional block.
+
+            Args:
+                filters: int
+                    The number of filters to use in the 3D convolutional
+                    block. The output layer of this green block will have
+                    this many number of channels.
+                input_shape: (int, int, int, int)
+                    The input shape required by tf.keras.layers.Conv3D being the first
+                    layer in this custom layer.
+                kernel_size: kernel_size: (int, int, int), optional
+                    The size of all convolutional kernels. Defaults to (3, 3, 3),
+                    as used in the paper.
+                data_format: str, optional
+                    The format of the input data. Must be either 'channels_last'
+                    or 'channels_first'. Defaults to `channels_last` for CPU
+                    development. 'channels_first is used in the paper.
+                groups: int, optional
+                    The size of each group for GroupNormalization. Defaults to
+                    8, as used in the paper.
+        """
         super(DecoderBlock, self).__init__()
 
         self.conv3d_ptwise = tf.keras.layers.Conv3D(
@@ -35,17 +61,43 @@ class DecoderBlock(tf.keras.layers.Layer):
                                 groups=groups)
 
     def call(self, x, enc_res):
+        """Returns the forward pass of one DecoderBlock.
+
+            { Conv3D_ptwise -> Upsample3D -> Residual -> ConvBlock }
+        """
         x = self.conv3d_ptwise(x)
         x = self.upsample(x)
         x = self.residual([x, enc_res])
         x = self.conv_block(x)
         return x
 
+
 class ConvDecoder(tf.keras.layers.Layer):
     def __init__(self,
                  data_format='channels_last',
                  kernel_size=(3, 3, 3),
                  groups=8):
+        """Initializes the model decoder.
+
+            See https://arxiv.org/pdf/1810.11654.pdf for more details.
+            The model decoder takes the encoder outputs at each level
+            of downsampling and upsamples, connecting each recovered
+            level with its corresponding encoder level residually. The
+            final logit layer outputs a 3-channel Tensor the same size
+            as the original input to the encoder.
+
+            Args:
+                data_format: str, optional
+                    The format of the input data. Must be either 'channels_last'
+                    or 'channels_first'. Defaults to `channels_last` for CPU
+                    development. 'channels_first is used in the paper.
+                kernel_size: kernel_size: (int, int, int), optional
+                    The size of all convolutional kernels. Defaults to (3, 3, 3),
+                    as used in the paper.
+                groups: int, optional
+                    The size of each group for GroupNormalization. Defaults to
+                    8, as used in the paper.
+        """
         super(ConvDecoder, self).__init__()
         self.dec_block_128 = DecoderBlock(
                                 filters=128,
@@ -82,6 +134,24 @@ class ConvDecoder(tf.keras.layers.Layer):
                                 activation='sigmoid')
 
     def call(self, enc_outs):
+        """Returns the forward pass of the ConvDecoder.
+
+            {
+                DecoderBlock_128 -> DecoderBlock_64 -> DecoderBlock_32,
+                OutputConv_32 -> LogitConv_3 + Sigmoid
+            }
+
+            Args:
+                enc_outs: List[Tensor]
+                    Contains residual outputs of the encoder from forward
+                    pass. Must contain (in order) the ConvBlock outputs at
+                    the 32, 64, 128, and 256 filter sizes.
+
+            Returns:
+                x: Tensor
+                    Output 'image' the same size as the original input image
+                    to the encoder, but with 3 channels.
+        """
         enc_out_32, enc_out_64, enc_out_128, x = enc_outs
 
         x = self.dec_block_128(x, enc_out_128)
