468a53daa0dbdbaf44ade6ec4d9b909001594435
Ademi Adeniji <ademi@stanford.edu>
2019-05-10 11:59:04 -0700

    Implemented VAE

--- debug.py
+++ debug.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from model.encoder import ConvEncoder
 from model.decoder import ConvDecoder
+from model.variational_autoencoder import VariationalAutoEncoder
 from utils.optimizer import ScheduledAdam
 
 def test_encoder(x):
@@ -41,6 +42,22 @@ def test_decoder(enc_outs):
     return logits
 
 
+def test_vae(enc_256):
+    """Tests that the encoder output shapes are correct."""
+    vae = VariationalAutoEncoder(
+        data_format='channels_last',
+        kernel_size=3,
+        groups=8,
+        kernel_regularizer=tf.keras.regularizers.l2(l=1e-5))
+
+    y_vae, z_mean, z_var = vae(enc_256)
+    assert(z_mean.shape == (1, 128))
+    assert(z_var.shape == (1, 128))
+    assert(y_vae.shape == (1, 160, 192, 128, 4))
+    print('All vae output shapes match.')
+
+    return (y_vae, z_mean, z_var)
+
 def test_optimizer():
     """Tests that the custom optimizer schedules correctly."""
     optimizer = ScheduledAdam(learning_rate=1e-4, n_epochs=300)
@@ -65,6 +82,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_decoder', action='store_true', default=False)
     parser.add_argument('--test_encoder', action='store_true', default=False)
+    parser.add_argument('--test_vae', action='store_true', default=False)
     parser.add_argument('--test_optimizer', action='store_true', default=False)
     args = parser.parse_args()
 
@@ -80,6 +98,10 @@ if __name__ == '__main__':
         enc_outs = (enc32, enc64, enc128, enc256)
 
         _ = test_decoder(enc_outs)
+
+    if args.test_vae:
+        enc256 = np.random.randn(1, 20, 24, 16, 256)
+        _ = test_vae(enc256)
 
     if args.test_optimizer:
         _ = test_optimizer()
--- model/variational_autoencoder.py
+++ model/variational_autoencoder.py
@@ -1,6 +1,53 @@
 import tensorflow as tf
+from model.group_norm import GroupNormalization
+from model.layers import ConvBlock
 
-# TODO: implement this class
+def sample(latent_args):
+    mean, var = latent_args
+    print(mean.shape)
+    print(var.shape)
+    batch_size = tf.keras.backend.shape(mean)[0]
+    dim = tf.keras.backend.shape(mean)[1]
+    eps = tf.keras.backend.random_normal(shape=(batch_size, dim), dtype='double')
+    print(eps.shape)
+    return mean + tf.keras.backend.exp(0.5*var) * eps
+
+class VariationalAutoEncoderBlock(tf.keras.layers.Layer):
+    def __init__(self,
+                 filters,
+                 kernel_size=3,
+                 data_format='channels_last',
+                 groups=8,
+                 kernel_regularizer=None):
+        super(VariationalAutoEncoderBlock, self).__init__()
+
+        self.conv3d_ptwise = tf.keras.layers.Conv3D(
+                                filters=filters,
+                                kernel_size=1,
+                                strides=1,
+                                padding='same',
+                                data_format=data_format,
+                                kernel_regularizer=kernel_regularizer)
+        self.upsample = tf.keras.layers.UpSampling3D(
+                                size=2,
+                                data_format=data_format)
+        self.conv_block = ConvBlock(
+                                filters=filters,
+                                kernel_size=kernel_size,
+                                data_format=data_format,
+                                groups=groups,
+                                kernel_regularizer=kernel_regularizer)
+
+    def call(self, x):
+        """Returns the forward pass of one DecoderBlock.
+
+            { Conv3D_ptwise -> Upsample3D -> Residual -> ConvBlock }
+        """
+        x = self.conv3d_ptwise(x)
+        x = self.upsample(x)
+        x = self.conv_block(x)
+        return x
+
 class VariationalAutoEncoder(tf.keras.layers.Layer):
     def __init__(self,
                  data_format='channels_last',
@@ -9,9 +56,111 @@ class VariationalAutoEncoder(tf.keras.layers.Layer):
                  kernel_regularizer=None):
         super(VariationalAutoEncoder, self).__init__()
 
+        # VD Block
+        self.groupnorm_256 = GroupNormalization(
+            groups=groups,
+            axis=-1 if data_format == 'channels_last' else 1)
+        self.relu_256 = tf.keras.layers.Activation('relu')
+        self.conv3d_256 = tf.keras.layers.Conv3D(
+            filters=16,
+            kernel_size=kernel_size,
+            strides=2,
+            padding='same',
+            data_format=data_format,
+            kernel_regularizer=kernel_regularizer)
+        self.flatten_256 = tf.keras.layers.Flatten(data_format)
+        self.proj_256 = tf.keras.layers.Dense(256)
+
+        # VDraw Block
+        self.mean = tf.keras.layers.Dense(128)
+        self.var = tf.keras.layers.Dense(128)
+        self.sample = tf.keras.layers.Lambda(sample)
+
+        # VU Block
+        self.proj_VU = tf.keras.layers.Dense(10*12*8*1)
+        self.relu_VU = tf.keras.layers.Activation('relu')
+        self.reshape = tf.keras.layers.Reshape((10, 12, 8, 1))
+        self.conv1d_VU = tf.keras.layers.Conv3D(
+            filters=256,
+            kernel_size=1,
+            strides=1,
+            padding='same',
+            data_format=data_format,
+            kernel_regularizer=kernel_regularizer
+        )
+        self.conv3d_upsample_VU = tf.keras.layers.UpSampling3D(
+            size=2,
+            data_format=data_format,
+        )
+
+        self.conv_block_128 = VariationalAutoEncoderBlock(
+            filters=128,
+            data_format=data_format,
+            groups=groups,
+            kernel_size=kernel_size,
+            kernel_regularizer=kernel_regularizer,
+        )
+
+        self.conv_block_64 = VariationalAutoEncoderBlock(
+            filters=64,
+            data_format=data_format,
+            groups=groups,
+            kernel_size=kernel_size,
+            kernel_regularizer=kernel_regularizer,
+        )
+
+        self.conv_block_32 = VariationalAutoEncoderBlock(
+            filters=32,
+            data_format=data_format,
+            groups=groups,
+            kernel_size=kernel_size,
+            kernel_regularizer=kernel_regularizer,
+        )
+
+        self.conv_4 = tf.keras.layers.Conv3D(
+            filters=4,
+            kernel_size=kernel_size,
+            strides=1,
+            padding='same',
+            data_format=data_format,
+            kernel_regularizer=kernel_regularizer,
+        )
+
     def call(self, x):
-        y_vae = None
-        z_mean = None
-        z_var = None
+
+        # VD Block
+        x = self.groupnorm_256(x)
+        x = self.relu_256(x)
+        x = self.conv3d_256(x)
+        x = self.flatten_256(x)
+        x = self.proj_256(x)
+
+        # VDraw Block
+        mu = self.mean(x)
+        z_mean = mu
+        sigma = self.var(x)
+        z_var = sigma
+        x = self.sample([mu, sigma])
+
+        # VU Block
+        x = self.proj_VU(x)
+        x = self.relu_VU(x)
+        x = self.reshape(x)
+        x = self.conv1d_VU(x)
+        x = self.conv3d_upsample_VU(x)
+
+        # VUp2 and VBlock2
+        x = self.conv_block_128(x)
+
+        # VUp1 and VBlock1
+        x = self.conv_block_64(x)
+
+        # VUp0 and VBlock0
+        x = self.conv_block_32(x)
+
+        # Vend
+        x = self.conv_4(x)
+
+        y_vae = x
+
         return y_vae, z_mean, z_var
-        
