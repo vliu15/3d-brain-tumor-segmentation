de164b1d382875d517775d1a14875f03f2efd6b3
Vincent Liu <vliu15@stanford.edu>
2019-05-25 13:42:52 -0700

    Fix CE / Focal losses

--- utils/losses.py
+++ utils/losses.py
@@ -19,13 +19,31 @@ class KullbackLeiblerLoss(tf.keras.losses.Loss):
         return tf.keras.losses.kld(y_true, y_pred)
 
 
-class CrossEntropyLoss(tf.keras.losses.CategoricalCrossentropy):
+class CrossEntropyLoss(tf.keras.losses.Loss):
+    """Implementation of balanced cross entropy loss."""
     def __init__(self,
                  smoothing=0.01,
+                 alpha=0.0,
                  name='cross_entropy_loss',
                  data_format='channels_last'):
+        """Initializes CrossEntropyLoss class and sets attributes needed to calculate loss.
+        
+            Args:
+                smoothing: float, optional
+                    amount of label smoothing to apply. Set to 0 for no smoothing.
+                alpha: float, optional
+                    amount of balance to apply (as in balanced cross entropy). Set
+                    to 0 for regular (unbalanced) cross entropy.
+                name: str, optional
+                    name of this loss class (for tf.Keras.losses.Loss).
+                data_format: str, optional
+                    format of the data, for determining the axis to compute loss over.
+        """
         super(CrossEntropyLoss, self).__init__(name=name)
+        assert smoothing <= 1 and smoothing >= 0, '`smoothing` needs to be in the range [0, 1].'
+        assert alpha <= 1 and alpha >= 0, '`alpha` needs to be in the range [0, 1].'
         self.smoothing = smoothing
+        self.alpha = alpha
         self.data_format = data_format
 
     def __call__(self, y_true, y_pred, sample_weight=None):
@@ -35,7 +53,17 @@ class CrossEntropyLoss(tf.keras.losses.CategoricalCrossentropy):
         if self.smoothing:
             y_true = y_true * (1.0 - self.smoothing) + (1 - y_true) * self.smoothing / (OUT_CH - 1)
 
-        return super(CrossEntropyLoss, self).__call__(y_true, y_pred, axis=axis)
+        # Prepare probabilities (may be unnecessary, but Keras backend does this).
+        y_pred = y_pred / tf.reduce_sum(y_pred, axis=axis, keepdims=True)
+        y_pred = tf.clip_by_value(y_pred, 1e-7, 1 - 1e-7)
+
+        # Apply p to y=1 and (1-p) to (y!=1) positions.
+        y_pred = y_true * y_pred + (1.0 - y_true) * (1.0 - y_pred)
+
+        # Apply self.alpha for class rebalancing.
+        alpha = y_true * self.alpha + (1.0 - y_true) * (1.0 - self.alpha)
+
+        return -tf.reduce_sum(alpha * tf.math.log(y_pred))
 
 
 class FocalLoss(tf.keras.losses.Loss):
@@ -46,8 +74,26 @@ class FocalLoss(tf.keras.losses.Loss):
                  alpha=0.25,
                  name='focal_loss',
                  data_format='channels_last'):
+        """Initializes FocalLoss class and sets attributes needed in loss calculation.
+
+            Args:
+                smoothing: float, optional
+                    amount of label smoothing to apply. Set to 0 for no smoothing.
+                gamma: int, optional
+                    amount of focal smoothing to apply. Set to 0 for regular
+                    balanced cross entropy.
+                alpha: float, optional
+                    amount of balance to apply (as in balanced cross entropy). Set
+                    to 0 for regular (unbalanced) cross entropy.
+                name: str, optional
+                    name of this loss class (for tf.Keras.losses.Loss).
+                data_format: str, optional
+                    format of the data, for determining the axis to compute loss over.
+        """
         super(FocalLoss, self).__init__(name=name)
-        assert alpha <= 1 and alpha >= 0
+        assert smoothing <= 1 and smoothing >= 0, '`smoothing` needs to be in the range [0, 1].'
+        assert alpha <= 1 and alpha >= 0, '`alpha` needs to be in the range [0, 1].'
+        assert gamma >= 0, '`gamma` needs to be a non-negative integer.'
         self.smoothing = smoothing
         self.gamma = gamma
         self.alpha = alpha
@@ -77,17 +123,16 @@ class FocalLoss(tf.keras.losses.Loss):
         alpha = y_true * self.alpha + (1.0 - y_true) * (1.0 - self.alpha)
 
         focus = (1 - y_pred) ** self.gamma
-        return -tf.reduce_sum(alpha * focus * y_true * tf.math.log(y_pred))
+        return -tf.reduce_sum(alpha * focus * tf.math.log(y_pred))
 
 
 class CustomLoss(tf.keras.losses.Loss):
+    """Custom loss class for weighted combinations of various losses."""
     def __init__(self,
                  name='custom_loss',
                  data_format='channels_last'):
         super(CustomLoss, self).__init__(name=name)
         self.focal_loss = FocalLoss(data_format=data_format)
-        self.generalized_dice_loss = GeneralizedDiceLoss(data_format=data_format)
 
     def __call__(self, y_true, y_pred, sample_weight=None):
-        return FL_WEIGHT * self.focal_loss(y_true, y_pred) + \
-                GDL_WEIGHT * self.generalized_dice_loss(y_true, y_pred)
+        return FL_WEIGHT * self.focal_loss(y_true, y_pred)
