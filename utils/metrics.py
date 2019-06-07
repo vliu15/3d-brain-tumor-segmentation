Fix hausdorff distance computation

utils/metrics.py
@@ -11,8 +11,14 @@ from utils.constants import *
 def pred_to_one_hot(pred, data_format):
     """Converts output of predicted probabilites to one-hot encodings."""
     axis = -1 if data_format == 'channels_last' else 1
+
+    # Mask out values that correspond to values < 0.5.
+    mask = tf.reduce_max(d1, axis=axis, keepdims=True)
+    mask = tf.cast(mask > 0.5, tf.int32)
+
     pred = tf.argmax(pred, axis=axis, output_type=tf.int32)
     pred = tf.one_hot(pred, OUT_CH, axis=axis, dtype=tf.float32)
+    pred *= mask
 
     return pred
 
@@ -24,17 +30,18 @@ class HausdorffDistance(tf.keras.metrics.Mean):
         super(HausdorffDistance, self).__init__(name=name)
 
     def __call__(self, y_true, y_pred, sample_weight=None):
-        y_pred = pred_to_one_hot(y_pred)
+        y_pred = pred_to_one_hot(y_pred, data_format=data_format)
 
         y_true = tf.reshape(y_true, shape=(1, -1))
         y_pred = tf.reshape(y_pred, shape=(1, -1))
         if directed_hausdorff:
-            haus_dist = tf.maximum(directed_hausdorff(y_true, y_pred)[0],
-                                directed_hausdorff(y_pred, y_true)[0])
+            haus_dist = tf.maximum(
+                            directed_hausdorff(y_true.numpy(), y_pred.numpy())[0],
+                            directed_hausdorff(y_pred.numpy(), y_true.numpy())[0])
             return super(HausdorffDistance, self).update_state(
                                 haus_dist, sample_weight=sample_weight)
         else:
-            raise super(HausdorffDistance, self).update_state(
+            return super(HausdorffDistance, self).update_state(
                                 float('inf'), sample_weight=sample_weight)
 
 
@@ -52,7 +59,7 @@ class DiceCoefficient(tf.keras.metrics.Mean):
         axis = (0, 1, 2, 3) if self.data_format == 'channels_last' else (0, 2, 3, 4)
         
         # Correct predictions will have 1, else 0.
-        y_pred = tf.cast(y_pred > 0.5, tf.float32)
+        y_pred = pred_to_one_hot(y_pred, data_format=data_format)
         intersection = tf.reduce_sum(y_pred * y_true, axis=axis)
 
         pred = tf.reduce_sum(y_pred, axis=axis)