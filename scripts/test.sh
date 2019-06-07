c4c8a3b1b3666c81b2d22ca7f581dfef9ac0461f
Vincent Liu <vliu15@stanford.edu>
2019-06-07 02:50:19 +0000

    Update scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,6 @@
 python test.py \
             --test_folder /share/pi/hackhack/test_viz \
             --data_format channels_first \
-            --chkpt_file chkpt-1.hdf5 \
+            --chkpt_file chkpt.hdf5 \
             --prepro_file /share/pi/hackhack/preprocessed/image_mean_std.npy \
             --gpu
--- scripts/train.sh
+++ scripts/train.sh
@@ -4,8 +4,8 @@ python train.py \
     --train_loc /share/pi/hackhack/preprocessed/train.image_wise.tfrecords \
     --val_loc /share/pi/hackhack/preprocessed/val.image_wise.tfrecords \
     --data_format channels_first \
-    --log_file train-8.log \
-    --save_file chkpt-8.hdf5 \
+    --log_file train.log \
+    --save_file chkpt.hdf5 \
     --log_steps -1 \
     --patience 10 \
     --n_epochs 300 \
